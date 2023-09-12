6548390da2f31e9b8b06d7e2037ad93c84879081
James Ahmad <JamesAhmad.dev@gmail.com>
2023-09-12 00:43:50 -0400

    init commit

---
+++ library_demo/src/main/groovy/com/example/library_demo/LibraryDemoApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.library_demo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class LibraryDemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(LibraryDemoApplication, args)
+	}
+
+}
