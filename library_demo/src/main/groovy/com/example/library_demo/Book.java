0e032ee1e35b873f8d6f1ef591f0a415b3b9618e
James Ahmad <JamesAhmad.dev@gmail.com>
2023-09-12 05:30:38 -0400

    fixed issue with Book object

--- library_demo/src/main/groovy/com/example/library_demo/Book.java
+++ library_demo/src/main/groovy/com/example/library_demo/Book.java
@@ -1,14 +1,10 @@
 package com.example.library_demo;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
+import java.util.Objects;
 
 @Entity
 public class Book {
@@ -23,46 +19,34 @@ public class Book {
     @ManyToMany
     private Collection<Author> authors;
 
-        
     public Long getId() {
         return id;
 
     }
-public Book(){
 
-}
-    public Book(String title, String description, Campus campus, Author ...authors) {
+    public Book() {
+
+    }
+
+    public Book(String title, String description, Campus campus, Author...authors) {
         this.title = title;
         this.description = description;
-        this.campus=campus;
-        this.authors=new ArrayList<>(Arrays.asList(authors));
+        this.campus = campus;
+        this.authors = new ArrayList<>(Arrays.asList(authors));
 
     }
+
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
+     return Objects.hash(id);
     }
+
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Book other = (Book) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Book book = (Book) obj;
+      return Objects.equals(id, book.id);
     }
-
-
-
 
 }
